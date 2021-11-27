b8fbf5673a5c9d86442fbc95c77f27ae66bb951e
Xamir Rojas <xamir.e.rojas@gmail.com>
2021-11-27 00:17:35 -0500

    People Model modification of fields hair_color and eye_color - Creation of HAIR_COLOR_CHOICES - Creation of EYE_COLOR_CHOICES - Migrations were made

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from django.db import models
 from model_utils.models import TimeStampedModel
 
@@ -30,10 +31,22 @@ class Planet(TimeStampedModel, SimpleNameModel):
 
 class People(TimeStampedModel, SimpleNameModel):
     """ Personajes del universo de Star Wars """
+    # Gender Choices
     MALE = 'male'
     FEMALE = 'female'
     HERMAPHRODITE = 'hermaphrodite'
     NA = 'n/a'
+    # Hair and Eye Color Choices
+    BLACK = 'black'
+    BROWN = 'brown'
+    BLONDE = 'blonde'
+    RED = 'red'
+    WHITE = 'white'
+    BALD = 'bald'
+    YELLOW = 'yellow'
+    GREEN = 'green'
+    PURPLE = 'purple'
+    UNKNOWN = 'unknown'
 
     GENDER = (
         (MALE, 'Male'),
@@ -42,14 +55,35 @@ class People(TimeStampedModel, SimpleNameModel):
         (NA, 'N/A'),
     )
 
+    HAIR_COLOR_CHOICES = (
+        (BLACK, 'Black'),
+        (BROWN, 'Brown'),
+        (BLONDE, 'Blonde'),
+        (RED, 'Red'),
+        (WHITE, 'White'),
+        (BALD, 'Bald')
+    )
+    EYE_COLOR_CHOICES = (
+        (BLACK, 'Black'),
+        (BROWN, 'Brown'),
+        (YELLOW, 'Yellow'),
+        (RED, 'Red'),
+        (GREEN, 'Green'),
+        (PURPLE, 'Purple'),
+        (UNKNOWN, 'Unknown'),
+    )
+
     height = models.CharField(max_length=16, blank=True)
     mass = models.CharField(max_length=16, blank=True)
-    hair_color = models.CharField(max_length=32, blank=True)
+    hair_color = models.CharField(
+        max_length=32, choices=HAIR_COLOR_CHOICES, blank=True)
     skin_color = models.CharField(max_length=32, blank=True)
-    eye_color = models.CharField(max_length=32, blank=True)
+    eye_color = models.CharField(
+        max_length=32, choices=EYE_COLOR_CHOICES, blank=True)
     birth_year = models.CharField(max_length=16, blank=True)
     gender = models.CharField(max_length=64, choices=GENDER)
-    home_world = models.ForeignKey(Planet, on_delete=models.CASCADE, related_name='inhabitants')
+    home_world = models.ForeignKey(
+        Planet, on_delete=models.CASCADE, related_name='inhabitants')
 
     class Meta:
         db_table = 'people'
@@ -75,9 +109,11 @@ class Film(TimeStampedModel):
     episode_id = models.PositiveSmallIntegerField()  # TODO: Agregar choices
     opening_crawl = models.TextField(max_length=1000)
     release_date = models.DateField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='films')
+    director = models.ForeignKey(
+        Director, on_delete=models.CASCADE, related_name='films')
     producer = models.ManyToManyField(Producer, related_name='films')
-    characters = models.ManyToManyField(People, related_name='films', blank=True)
+    characters = models.ManyToManyField(
+        People, related_name='films', blank=True)
     planets = models.ManyToManyField(Planet, related_name='films', blank=True)
 
     class Meta:
