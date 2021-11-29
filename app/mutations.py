fa0abc63b75fc992e52e46c00eb61347f02dd9dd
Xamir Rojas <xamir.e.rojas@gmail.com>
2021-11-28 23:00:17 -0500

    generic model mutation process function DocString mutations.py comments

--- app/mutations.py
+++ app/mutations.py
@@ -11,6 +11,7 @@ from .utils import generic_model_mutation_process
 
 class AddPlanetMutation(graphene.relay.ClientIDMutation):
     class Input:
+        # Input arguments for the mutation
         id = graphene.ID(required=False)
         name = graphene.String(required=True)
         rotation_period = graphene.String(required=False)
@@ -22,15 +23,21 @@ class AddPlanetMutation(graphene.relay.ClientIDMutation):
         surface_water = graphene.String(required=False)
         population = graphene.String(required=False)
 
+    # response of the mutation
     planet = graphene.Field(PlanetType)
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
+        # Gets id from input or leaves it in None
         raw_id = input.get('id', None)
 
         data = {'model': Planet, 'data': input}
+        #
         if raw_id:
+            # from_global_id :
+            # takes the raw_id and returns the Type name and ID used to create it
+            # saves in data.id the ID of the Type
             data['id'] = from_global_id(raw_id)[1]
-
         planet = generic_model_mutation_process(**data)
+        # Returns Planet instance
         return AddPlanetMutation(planet=planet)
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,21 @@
+
+
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    """
+    Modify a Database item or creates it if it doesn't exist :
+
+    - If there is ID, gets the related item from the database 
+    Tries to delete the id attribute from the data dictionary
+    Set the data values to the field of the item using a for loop
+    - If there is no ID, creates a new Item using the data
+
+    Finally saves the object back to the database 
+    """
+
     if id:
+        # get item
         item = model.objects.get(id=id)
+        # set the value of field to value of the item object
         try:
             del data['id']
         except KeyError:
@@ -9,9 +24,11 @@ def generic_model_mutation_process(model, data, id=None, commit=True):
         for field, value in data.items():
             setattr(item, field, value)
     else:
+        # Creates the object using the data
         item = model(**data)
 
     if commit:
+        # saves the object back to the database
         item.save()
 
     return item
