1684939999c82b23d83e11dba388b878933cb01d
Xamir Rojas <xamir.e.rojas@gmail.com>
2021-11-30 21:58:47 -0500

    Creation of Character Test

--- app/tests.py
+++ app/tests.py
@@ -2,7 +2,11 @@ import json
 
 from graphene_django.utils.testing import GraphQLTestCase
 
+from graphql_relay import from_global_id, to_global_id
+
 from swapi.schema import schema
+from .models import Planet, Film
+from .types import PlanetType, FilmType
 
 
 class FirstTestCase(GraphQLTestCase):
@@ -28,3 +32,46 @@ class FirstTestCase(GraphQLTestCase):
 
         content = json.loads(response.content)
         self.assertEqual(len(content['data']['allPlanets']['edges']), 61)
+
+    def test_create_people(self):
+        response = self.query(
+            '''
+            mutation createPeopleMutation($people:CreatePeopleMutationInput!){
+              createPeopleMutation(input:$people){
+                people{
+                  name,
+                  height,
+                  homeWorld{
+                    id,
+                  }
+                }
+              }
+            }
+        ''', variables={
+                'people': {
+                    'name': "new player",
+                    'height': 1800,
+                    'homeWorld': "UGxhbmV0VHlwZTox",
+                    'films': [
+                        {'id': "RmlsbVR5cGU6MQ=="}
+                    ]
+                }
+            }
+        )
+        self.assertResponseNoErrors(response)
+        # converts json response to python object
+        content = json.loads(response.content)
+        expected_response = {
+            "data": {
+                "createPeopleMutation": {
+                    "people": {
+                        "name": "new player",
+                        "height": "1800",
+                        "homeWorld": {
+                            "id": "UGxhbmV0VHlwZTox",
+                        }
+                    }
+                }
+            }
+        }
+        self.assertEqual(expected_response, content)
